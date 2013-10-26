f86fad05401986f3eec0cf39d852c4f2a17be92e
Dan Matthews <dan@danmatthews.me>
2013-10-26 21:49:22 +0100

    Added some descriptions to the config file

--- src/config/config.php
+++ src/config/config.php
@@ -1,12 +1,34 @@
 <?php
 
 return array(
-    /**
-     * Cache the ACL rules once registered?
-     * This will cache them indefinitely, and each time you update
-     * your permissions config, you will need to run
-     * `php artisan permissions:refresh`
-     */
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cache provider
+    |--------------------------------------------------------------------------
+    |
+    | When laravel boots up Sanction, it will by default run through all your
+    | rules in the `roles.php` config file. Once it has set these up, it will cache the
+    | resulting Zend Acl Object instance for faster loads.
+    |
+    | The default provider uses Laravel's `Cache` methods/facade.
+    | If you don't wish to do any caching, set this to `null`.
+    |
+    */
     'cache_provider' => 'Curlymoustache\Sanction\Cache\LaravelSanctionCacheProvider',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Role Lookup Provider
+    |--------------------------------------------------------------------------
+    |
+    | In order to stay as flexible as possible, Sanction allows you to customise
+    | how it looks for roles against users.
+    |
+    | The LaravelDatabaseLookupProvider uses the migration provided with this package
+    | and assumes that you have `roles` database table with `user_id`(integer)
+    | and `role_id` (varchar) columns.
+    |
+    */
     'role_lookup_provider' => 'Curlymoustache\Sanction\RoleLookup\LaravelDatabaseLookupProvider',
 );
