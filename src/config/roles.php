9f192e7206a3af54f0a1f56bac453810f5e54cf3
Dan Matthews <dan@danmatthews.me>
2013-10-26 21:57:28 +0100

    added some descriptions to the roles file

--- src/config/roles.php
+++ src/config/roles.php
@@ -1,25 +1,48 @@
 <?php
 
+/*
+|--------------------------------------------------------------------------
+| Setting up roles...
+|--------------------------------------------------------------------------
+|
+| Each array key should be a role slug, using underscores instead of spaces,
+| you should try and keep these simple and lowercase.
+|
+|
+|
+| The value of each item should be an array with the following keys:
+|
+| $permissions - A one dimensional array of permission names as strings.
+| $inherits_from - This should be a list of valid role names to
+|  inherit permissions from.
+|
+| The dot syntax (users.create) inside the permissions arrays has no
+| special significance - it's simply how i choose to write my default
+| rules, you could seperate names with underscores or anything, really, like:
+|
+| 'create_users', 'update_users', 'user|create', 'user:update'
+|
+*/
 return array(
-
     'super_administrator' => array(
-        'display_name' => 'Administrator',
         'permissions' => array(
-            'news.create'
+            'users.create',
+            'users.update',
+            'users.delete',
         ),
         'inherits_from' => array('standard_user', 'administrator'),
     ),
     'standard_user' => array(
-        'display_name' => 'Standard user',
         'permissions' => array(
-            'user.add',
-            'user.create',
+            'pages.add',
+            'pages.update',
+            'self.update',
         ),
     ),
     'administrator' => array(
-        'display_name' => 'Administrator',
         'permissions' => array(
-            'user.publish'
+            'users.create',
+            'users.update'
         ),
         'inherits_from' => array('standard_user'),
     ),
